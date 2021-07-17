fbc2d3cdc3010d0d07db4ff2f15d892536aba15c
josecortazar <jose.cortazar@ceiba.com.co>
2021-07-17 09:35:03 -0500

    Libreria de Mockito

--- microservicio/dominio/src/test/java/com/ceiba/usuario/servicio/ServicioCrearUsuarioTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/usuario/servicio/ServicioCrearUsuarioTest.java
@@ -5,6 +5,9 @@ import com.ceiba.usuario.puerto.repositorio.RepositorioUsuario;
 import com.ceiba.dominio.excepcion.ExcepcionDuplicidad;
 import com.ceiba.dominio.excepcion.ExcepcionLongitudValor;
 import com.ceiba.usuario.servicio.testdatabuilder.UsuarioTestDataBuilder;
+
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -12,22 +15,29 @@ import com.ceiba.BasePrueba;
 
 public class ServicioCrearUsuarioTest {
 
-    @Test
-    public void validarClaveLongitudMenor4Test() {
-        // arrange
-        UsuarioTestDataBuilder usuarioTestDataBuilder = new UsuarioTestDataBuilder().conClave("124");
-        // act - assert
-        BasePrueba.assertThrows(() -> usuarioTestDataBuilder.build(), ExcepcionLongitudValor.class, "La clave debe tener una longitud mayor o igual a 4");
-    }
+	@Test
+	public void validarClaveLongitudMenor4Test() {
+		// arrange
+		UsuarioTestDataBuilder usuarioTestDataBuilder = new UsuarioTestDataBuilder().conClave("124");
+		// act - assert
+		BasePrueba.assertThrows(() -> usuarioTestDataBuilder.build(), ExcepcionLongitudValor.class,
+				"La clave debe tener una longitud mayor o igual a 4");
+	}
 
-    @Test
-    public void validarUsuarioExistenciaPreviaTest() {
-        // arrange
-        Usuario usuario = new UsuarioTestDataBuilder().build();
-        RepositorioUsuario repositorioUsuario = Mockito.mock(RepositorioUsuario.class);
-        Mockito.when(repositorioUsuario.existe(Mockito.anyString())).thenReturn(true);
-        ServicioCrearUsuario servicioCrearUsuario = new ServicioCrearUsuario(repositorioUsuario);
-        // act - assert
-        BasePrueba.assertThrows(() -> servicioCrearUsuario.ejecutar(usuario), ExcepcionDuplicidad.class,"El usuario ya existe en el sistema");
-    }
+	@Test
+	public void validarUsuarioExistenciaPreviaTest() {
+		// arrange
+		Usuario usuario = new UsuarioTestDataBuilder().build();
+
+		RepositorioUsuario repositorioUsuario = Mockito.mock(RepositorioUsuario.class);
+
+		Mockito.when(repositorioUsuario.existe(Mockito.anyString())).thenReturn(true);
+
+		ServicioCrearUsuario servicioCrearUsuario = new ServicioCrearUsuario(repositorioUsuario);
+
+		// act - assert
+		BasePrueba.assertThrows(() -> servicioCrearUsuario.ejecutar(usuario), ExcepcionDuplicidad.class,
+				"El usuario ya existe en el sistema");
+	}
+
 }
