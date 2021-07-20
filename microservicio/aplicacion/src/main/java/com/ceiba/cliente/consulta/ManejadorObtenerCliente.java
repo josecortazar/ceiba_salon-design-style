53d34661bf9a8476ca822c2f6dbde8c1c0e011f4
josecortazar <jose.cortazar@ceiba.com.co>
2021-07-19 20:21:31 -0500

    Excepcion

--- microservicio/aplicacion/src/main/java/com/ceiba/cliente/consulta/ManejadorObtenerCliente.java
+++ microservicio/aplicacion/src/main/java/com/ceiba/cliente/consulta/ManejadorObtenerCliente.java
@@ -4,8 +4,7 @@ import org.springframework.stereotype.Component;
 
 import com.ceiba.cliente.modelo.dto.DtoCliente;
 import com.ceiba.cliente.puerto.dao.DaoCliente;
-import com.ceiba.dominio.excepcion.ExcepcionSinDatos;
-import com.ceiba.excepcion.ExcepcionDatoNoEncontrado;
+import com.ceiba.excepcion.ExcepcionSinDatos;
 
 @Component
 public class ManejadorObtenerCliente {
