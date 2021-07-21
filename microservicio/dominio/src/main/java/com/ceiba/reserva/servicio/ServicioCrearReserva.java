2d21ee9a16c7cfc9ccfb51383ec283d8d8e0e4a2
josecortazar <jose.cortazar@ceiba.com.co>
2021-07-21 14:29:39 -0500

    Excepcion en reservas

--- microservicio/dominio/src/main/java/com/ceiba/reserva/servicio/ServicioCrearReserva.java
+++ microservicio/dominio/src/main/java/com/ceiba/reserva/servicio/ServicioCrearReserva.java
@@ -6,6 +6,9 @@ import com.ceiba.dominio.excepcion.ExcepcionNoEncontrado;
 
 import static com.ceiba.dominio.ValidadorArgumento.validarMenorEdad;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import com.ceiba.reserva.modelo.entidad.Reserva;
 import com.ceiba.reserva.puerto.repositorio.RepositorioReserva;
 
@@ -14,7 +17,6 @@ public class ServicioCrearReserva {
 	private static final String LA_SERVICIO_YA_EXISTE_EN_EL_SISTEMA = "La reserva ya existe en el sistema";
 	private static final String EL_CLIENTE_NO_SE_ENCONTRO_EN_EL_SISTEMA = "El cliente no se encontro en el sistema";
 
-	
 	private final RepositorioReserva repositorioReserva;
 	private final DaoCliente daoCliente;
 
@@ -40,6 +42,10 @@ public class ServicioCrearReserva {
 		try {
 			return validarMenorEdad(daoCliente.obtener(reserva.getIdCliente()).getFechaNacimiento());
 		} catch (Exception e) {
+			Logger logger = Logger.getLogger(ServicioCrearReserva.class.getName());
+
+			logger.log(Level.WARNING, "logging: {0} ", EL_CLIENTE_NO_SE_ENCONTRO_EN_EL_SISTEMA);
+
 			throw new ExcepcionNoEncontrado(EL_CLIENTE_NO_SE_ENCONTRO_EN_EL_SISTEMA);
 		}
 	}
