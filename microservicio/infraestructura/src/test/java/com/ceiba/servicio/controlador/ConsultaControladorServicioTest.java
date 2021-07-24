a25787e62abd9d33c7a8e3c16791714c0199f6f6
josecortazar <jose.cortazar@ceiba.com.co>
2021-07-24 13:56:27 -0500

    Test de integracion de servicios

--- microservicio/infraestructura/src/test/java/com/ceiba/servicio/controlador/ConsultaControladorServicioTest.java
+++ microservicio/infraestructura/src/test/java/com/ceiba/servicio/controlador/ConsultaControladorServicioTest.java
@@ -30,7 +30,7 @@ public class ConsultaControladorServicioTest {
 	public void listarServicios() throws Exception {
 		// arrange act - assert
 		mocMvc.perform(get("/servicios").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
-				.andExpect(jsonPath("$", hasSize(1))).andExpect(jsonPath("$[0].idServicio", is(1)));
+				.andExpect(jsonPath("$", hasSize(1))).andExpect(jsonPath("$[0].id", is(1)));
 	}
 
 	@Test
@@ -38,7 +38,7 @@ public class ConsultaControladorServicioTest {
 		// arrange act - assert
 		Long id = 1L;
 		mocMvc.perform(get("/servicios/{id}", id).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
-				.andExpect(jsonPath("idServicio", is(1)));
+				.andExpect(jsonPath("id", is(1)));
 	}
 
 }
